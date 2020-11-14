54f36ec79e823e9acc8e76f7a51f3c525fac22f9
sorenhoyer <sorenhoyer84@gmail.com>
2020-11-14 16:09:18 +0100

    ...

--- server/graphql/src/database.ts
+++ server/graphql/src/database.ts
@@ -1,8 +1,8 @@
 type Foo = { id: string; text: string };
 
 const foos: Record<string, Foo> = {
-  'cdfda462-7fe0-4d15-ae38-164e92040f19': { id: 'cdfda462-7fe0-4d15-ae38-164e92040f19', text: 'bar' },
-  '6728dbdc-8f47-444c-adc3-c9e707e2144a': { id: '6728dbdc-8f47-444c-adc3-c9e707e2144a', text: 'baz' },
+  'cdfda462-7fe0-4d15-ae38-164e92040f19': { id: 'cdfda462-7fe0-4d15-ae38-164e92040f19', text: 'Foo 1' },
+  '6728dbdc-8f47-444c-adc3-c9e707e2144a': { id: '6728dbdc-8f47-444c-adc3-c9e707e2144a', text: 'Foo 2' },
   '73276921-f0a2-401c-8438-12f4c4fb1529': { id: '73276921-f0a2-401c-8438-12f4c4fb1529', text: 'Foo 3' },
   '51d71006-be69-450f-af7f-78945c4a7d2c': { id: '51d71006-be69-450f-af7f-78945c4a7d2c', text: 'Foo 4' },
   '71bc3c38-4405-4809-970b-0bdacae04e26': { id: '71bc3c38-4405-4809-970b-0bdacae04e26', text: 'Foo 5' },
